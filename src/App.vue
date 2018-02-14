c52d05d4cac0d54d0f7660eb55cff1755233d352
Samuelson Brito <samuelsonma@hotmail.com>
2018-02-13 20:29:08 -0400

    requisições com axios

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  	<lv-lista titulo="Aviões" :lista="avioes"></lv-lista>
+  	<lv-lista titulo="Carros" :lista="carros"></lv-lista>
+  </div>
+</template>
+
+<script>
+
+import Carro from './servicos/carros'
+import Aviao from './servicos/avioes'
+import LvLista from './Lista.vue'
+
+export default {
+  name: 'app',
+  data(){
+  	return {
+  		avioes: '',
+  		carros: ''
+  	}
+  },
+  components:{LvLista},
+  mounted(){
+    Carro.lista().then(dado => this.carros = dado.data)
+    Aviao.lista().then(dado => this.avioes = dado.data)
+  }
+}
+</script>
---
+++ src/Lista.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h4>Lista de {{ titulo }}</h4>
+		<ul>
+			<li v-for="item in lista">
+				{{ item.nome }} de <i>{{ item.marca }}</i>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		props: {
+			titulo: null,
+			lista: {
+				required: true,
+				type: Array | Object
+			}
+		}
+	}
+</script>
